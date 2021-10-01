c60858ca0ee50ae3ab0773ecb4a106887bbc6f8f
Žiga Vehovec <ziga.veho@gmail.com>
2021-10-01 11:30:50 +0200

    Example test

--- app/src/androidTest/java/work/vehovec/testapplication/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/work/vehovec/testapplication/ExampleInstrumentedTest.kt
@@ -1,12 +1,23 @@
 package work.vehovec.testapplication
 
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.android.material.button.MaterialButton
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.instanceOf
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,5 +31,24 @@ class ExampleInstrumentedTest {
     // Context of the app under test.
     val appContext = InstrumentationRegistry.getInstrumentation().targetContext
     assertEquals("work.vehovec.testapplication", appContext.packageName)
+  }
+
+  @Rule
+  @JvmField
+  var activityTestRule = ActivityScenarioRule(MainActivity::class.java)
+
+  @Test
+  fun nextButtonClickTest() {
+    onView(withId(R.id.button_first)).perform(click())
+
+    Thread.sleep(100)
+
+    onView(`is`(instanceOf(MaterialButton::class.java))).check(matches(withText("PREVIOUS")))
+
+    onView(withId(R.id.button_second)).perform(click())
+
+    Thread.sleep(100)
+
+    onView(`is`(instanceOf(MaterialButton::class.java))).check(matches(withText("NEXT")))
   }
 }
